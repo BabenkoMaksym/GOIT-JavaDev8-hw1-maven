ea220dfed3c53f722f510832779426e460741073
BabenkoMaksym <Maksym.Babenko.bbn@gmail.com>
2022-12-25 21:58:21 +0100

    first commit

---
+++ src/main/java/ua/goit/dev8/homework1/Main.java
@@ -0,0 +1,12 @@
+package ua.goit.dev8.homework1;
+
+import com.google.gson.Gson;
+
+public class Main {
+    public static void main(String[] args) {
+        Person person = new Person("Maksym", "Babenko");
+        Gson gson = new Gson();
+        String personInString = gson.toJson(person);
+        System.out.println(personInString);
+    }
+}
---
+++ src/main/java/ua/goit/dev8/homework1/Person.java
@@ -0,0 +1,11 @@
+package ua.goit.dev8.homework1;
+
+public class Person {
+    private final String name;
+    private final String lastName;
+
+    public Person(String name, String lastName) {
+        this.name = name;
+        this.lastName = lastName;
+    }
+}
